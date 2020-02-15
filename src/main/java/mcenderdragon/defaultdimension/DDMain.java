8adedf35f6714e6f37f9407646e6c0a9b4a32bbf
mcenderdragon <goto.minecraft@googlemail.com>
2020-02-15 15:55:49 +0100

    on first spawn player gets ported into the correct dimension. Player respawn in respawnable dimension else in default dimension.

--- src/main/java/mcenderdragon/defaultdimension/DDMain.java
+++ src/main/java/mcenderdragon/defaultdimension/DDMain.java
@@ -40,154 +40,160 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 public class DDMain 
 {
 	// Directly reference a log4j logger.
-    public static final Logger LOGGER = LogManager.getLogger();
+	public static final Logger LOGGER = LogManager.getLogger();
 	public static final String MOD_ID = "defaultdimension";
 
-    public DDMain() 
-    {
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+	public DDMain() 
+	{
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
 
-        // Register ourselves for server and other game events we are interested in
-        MinecraftForge.EVENT_BUS.register(this);
-        
-        DDConfig.registerConfig(ModLoadingContext.get().getActiveContainer());
-    }
+		// Register ourselves for server and other game events we are interested in
+		MinecraftForge.EVENT_BUS.register(this);
+		
+		DDConfig.registerConfig(ModLoadingContext.get().getActiveContainer());
+	}
 
-    /**
-     * Preinit
-     */
-    private void setup(final FMLCommonSetupEvent event)
-    {
-        // preinit code
-       
-    }
+	/**
+	 * Preinit
+	 */
+	private void setup(final FMLCommonSetupEvent event)
+	{
+		// preinit code
+	   
+	}
 
-    private void doClientStuff(final FMLClientSetupEvent event) 
-    {
-        
-    }
+	private void doClientStuff(final FMLClientSetupEvent event) 
+	{
+		
+	}
 
-    private void enqueueIMC(final InterModEnqueueEvent event)
-    {
-        // some example code to dispatch IMC to another mod
-//        InterModComms.sendTo("examplemod", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
-    }
+	private void enqueueIMC(final InterModEnqueueEvent event)
+	{
+		// some example code to dispatch IMC to another mod
+//		InterModComms.sendTo("examplemod", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
+	}
 
-    private void processIMC(final InterModProcessEvent event)
-    {
-        // some example code to receive and process InterModComms from other mods
-//        LOGGER.info("Got IMC {}", event.getIMCStream().
-//                map(m->m.getMessageSupplier().get()).
-//                collect(Collectors.toList()));
-    }
+	private void processIMC(final InterModProcessEvent event)
+	{
+		// some example code to receive and process InterModComms from other mods
+//		LOGGER.info("Got IMC {}", event.getIMCStream().
+//				map(m->m.getMessageSupplier().get()).
+//				collect(Collectors.toList()));
+	}
    
-    @SubscribeEvent
-    public void onServerStarting(FMLServerStartingEvent event) 
-    {
-       
-//        LOGGER.info("HELLO from server starting");
-    }
+	@SubscribeEvent
+	public void onServerStarting(FMLServerStartingEvent event) 
+	{
+	   CommandDimensinalSpread.register(event.getCommandDispatcher());
+//		LOGGER.info("HELLO from server starting");
+	}
 
-    @SubscribeEvent
-    public void onPlayeRespawn(PlayerEvent.PlayerRespawnEvent event)
-    {
-    	
-    }
-    
-    @SubscribeEvent
-    public void onPlayeChangeDimension(PlayerEvent.PlayerChangedDimensionEvent event)
-    {
-    	
-    }
-    
-    @SubscribeEvent
-    public void onPlayeCloned(PlayerEvent.Clone event)
-    {
-    	if(event.getPlayer() instanceof ServerPlayerEntity)
+	@SubscribeEvent
+	public void onPlayeRespawn(PlayerEvent.PlayerRespawnEvent event)
+	{
+		
+	}
+	
+	@SubscribeEvent
+	public void onPlayeChangeDimension(PlayerEvent.PlayerChangedDimensionEvent event)
+	{
+		
+	}
+	
+	@SubscribeEvent
+	public void onPlayeCloned(PlayerEvent.Clone event)
+	{
+		if(event.getPlayer() instanceof ServerPlayerEntity)
 		{
 			ServerPlayerEntity pl = (ServerPlayerEntity) event.getPlayer();
 			
-			CompoundNBT nbt = pl.getPersistentData().getCompound(PlayerEntity.PERSISTED_NBT_TAG);
-			CompoundNBT modData = nbt.getCompound(MOD_ID);
-			
-			if(modData.getBoolean("isSpawnDimensionSet"))
+			if(pl.getSpawnDimension() == DimensionType.OVERWORLD)
 			{
-				event.getPlayer().setSpawnDimenion(event.getOriginal().getSpawnDimension());
-				
+				pl.setSpawnDimenion(event.getOriginal().getSpawnDimension());
 			}
-    	
-			
 		}
-    }
-    
-    @SubscribeEvent
-    public void onEntitySpawnFirst(EntityJoinWorldEvent event)
-    {
-    	Entity e = event.getEntity();
-    	if(!e.world.isRemote)
-    	{
-    		if(e instanceof ServerPlayerEntity)
-    		{
-    			ServerPlayerEntity pl = (ServerPlayerEntity) event.getEntity();
-    			
-    			CompoundNBT nbt = pl.getPersistentData().getCompound(PlayerEntity.PERSISTED_NBT_TAG);
-    			CompoundNBT modData = nbt.getCompound(MOD_ID);
-    			
-    			if(!modData.getBoolean("isSpawnDimensionSet"))
-    			{
-    				DimensionType type = DDConfig.COMMON.getDefaultDimension();
-    				if(pl.getSpawnDimension()!=type)
-    				{
-    					pl.setSpawnDimenion(type);
-    				}
-    				
-    				if(pl.dimension != type)
-    				{
-    					MinecraftServer server = pl.getServer();
-    					ServerWorld world = server.getWorld(type);
-    					
-    					LOGGER.info("Changing Player {} dimension to {}",pl,  type);
-    					pl.teleport(world, pl.posX, pl.posY, pl.posZ, pl.rotationYaw, pl.rotationPitch);
-    					
-    					BlockPos spawn = pl.getBedLocation(type);
-    					boolean forced = pl.isSpawnForced(type);
-    					Optional<Vec3d> optional;
-    					if(spawn!=null && World.isValid(spawn))
-    					{
-    						optional = PlayerEntity.func_213822_a(world, spawn, forced);
-    					}
-    					else
-    					{
-    						optional = Optional.empty();
-    					}
-    					BlockPos worldSpawn = world.getSpawnPoint();
-    					Vec3d spawnPos = optional.orElse(new Vec3d(worldSpawn.getX() + 0.5, worldSpawn.getY() + 0.2, worldSpawn.getZ() + 0.5));
-    					pl.setPosition(spawnPos.getX(), spawnPos.getY(), spawnPos.getZ());
-    					LOGGER.info("Seting player {} position to {}", pl, spawnPos);
-    					pl.addPotionEffect(new EffectInstance(Effects.RESISTANCE, 20 * 5, 20, true, false));
-    				}
-    				
-    				modData.putBoolean("isSpawnDimensionSet", true);
-    			}
-    			
-    			nbt.put(MOD_ID, modData);
-    			pl.getPersistentData().put(PlayerEntity.PERSISTED_NBT_TAG, nbt);
-    		}
-    	}
-    	
-    }
-    
-    
-    @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
-    public static class RegistryEvents 
-    {
-        @SubscribeEvent
-        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
-            // register a new block here
-            LOGGER.info("HELLO from Register Block");
-        }
-    }
+	}
+	
+	@SubscribeEvent
+	public void onEntitySpawnFirst(EntityJoinWorldEvent event)
+	{
+		Entity e = event.getEntity();
+		if(!e.world.isRemote)
+		{
+			if(e instanceof ServerPlayerEntity)
+			{
+				ServerPlayerEntity pl = (ServerPlayerEntity) event.getEntity();
+				DimensionType type = DDConfig.COMMON.getDefaultDimension();
+				
+				if(pl.getSpawnDimension() == DimensionType.OVERWORLD && type != DimensionType.OVERWORLD)
+				{
+					pl.setSpawnDimenion(type);
+				}
+				
+				
+				CompoundNBT nbt = pl.getPersistentData().getCompound(PlayerEntity.PERSISTED_NBT_TAG);
+				CompoundNBT modData = nbt.getCompound(MOD_ID);
+				//modData.putBoolean("isSpawnDimensionSet", false);
+				
+				if(!modData.getBoolean("isSpawnDimensionSet"))
+				{
+					
+					if(pl.getSpawnDimension()!=type)
+					{
+						pl.setSpawnDimenion(type);
+					}
+					
+					if(pl.dimension != type)
+					{
+						MinecraftServer server = pl.getServer();
+						ServerWorld world = server.getWorld(type);
+						respawnIn(world, pl);
+					}
+					
+					modData.putBoolean("isSpawnDimensionSet", true);
+				}
+				
+				nbt.put(MOD_ID, modData);
+				pl.getPersistentData().put(PlayerEntity.PERSISTED_NBT_TAG, nbt);
+			}
+		}
+		
+	}
+	
+	public static void respawnIn(ServerWorld world, ServerPlayerEntity pl)
+	{
+		LOGGER.info("Changing Player {} dimension to {}",pl, world.dimension.getType());
+		pl.teleport(world, pl.posX, pl.posY, pl.posZ, pl.rotationYaw, pl.rotationPitch);
+		
+		BlockPos spawn = pl.getBedLocation(world.dimension.getType());
+		boolean forced = pl.isSpawnForced(world.dimension.getType());
+		Optional<Vec3d> optional;
+		if(spawn!=null && World.isValid(spawn))
+		{
+			optional = PlayerEntity.func_213822_a(world, spawn, forced);
+		}
+		else
+		{
+			optional = Optional.empty();
+		}
+		BlockPos worldSpawn = world.getSpawnPoint();
+		Vec3d spawnPos = optional.orElse(new Vec3d(worldSpawn.getX() + 0.5, worldSpawn.getY() + 0.2, worldSpawn.getZ() + 0.5));
+		pl.teleport(world, spawnPos.getX(), spawnPos.getY(), spawnPos.getZ(), pl.rotationYaw, pl.rotationPitch);
+		LOGGER.info("Seting player {} position to {}", pl, spawnPos);
+		pl.addPotionEffect(new EffectInstance(Effects.RESISTANCE, 20 * 5, 20, true, false));
+	}
+	
+	
+	@Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
+	public static class RegistryEvents 
+	{
+		@SubscribeEvent
+		public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
+			// register a new block here
+			LOGGER.info("HELLO from Register Block");
+		}
+	}
 }
